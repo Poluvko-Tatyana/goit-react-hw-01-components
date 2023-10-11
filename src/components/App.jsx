2a0fb22f64e5208c3a453c705e53828240494773
Poluvko-Tatyana <poluvko.tatyana@gmail.com>
2023-10-11 17:42:31 +0300

    дз 1-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
+import React from 'react';
+import user from '../components/all-json/user';
+import data from '../components/all-json/data';
+import friends from '../components/all-json/friends';
+import transactions from '../components/all-json/transactions';
+import { Profiler } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { FriendListItem } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+
+
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <div>
+  <Profiler
+username={user.username}
+tag={user.tag}
+location={user.location}
+avatar={user.avatar}
+stats={user.stats}
+/>
+
+<Statistics title="Upload stats" stats={data} />
+
+<ul>
+        {friends.map(friend => {
+          return (
+            <FriendListItem 
+            friend={friend}
+            key = {friend.id} />
+          );
+        })}
+</ul>
+
+<TransactionHistory items={transactions} />;
+
+</div>
   );
 };
+
