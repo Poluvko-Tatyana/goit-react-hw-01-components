3aae7ee331226d1492af13f8a4ac23e2b0b6f609
Poluvko-Tatyana <poluvko.tatyana@gmail.com>
2023-10-18 15:12:10 +0300

    дз1

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,7 +3,7 @@ import css from '../Statistics/Statistics.module.css';
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      <h2 className="title">{title}</h2>
+     { title ? <h2>{title}</h2> : null}
       <ul className={css.stat_list}>
         {stats.map(stat => {
           return (
